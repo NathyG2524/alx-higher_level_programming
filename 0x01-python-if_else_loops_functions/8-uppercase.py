a1fec93ae6e9ae9373180b80d8683d42720daed6
NathyG2524 <Nathygirma2524@gmail.com>
2021-11-16 14:01:13 -0800

    change to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for x in str:
+        if ord(x) >= 97 and ord(x) <= 122:
+            print(chr((ord(x)-97)+65), end="")
+        else:
+            print(x, end="")
+    print()
