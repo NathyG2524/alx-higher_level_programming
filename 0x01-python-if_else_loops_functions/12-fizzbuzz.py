8831adc2bb6ab0eb7f4171cb90cfdbebde3bbc9c
NathyG2524 <Nathygirma2524@gmail.com>
2021-11-16 15:35:36 -0800

    FizzBuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-for x in range(101):
-    if x % 3 == 0:
-        print('Fizz')
-    elif x % 5 == 0:
-        print('Buzz')
-    elif x % 3 == 0 and x % 5 == 0:
-        print('FizzBuzz')
-    else:
-        print(x)
+    for x in range(101):
+        if x % 3 == 0:
+            print('Fizz')
+        elif x % 5 == 0:
+            print('Buzz')
+        elif x % 3 == 0 and x % 5 == 0:
+            print('FizzBuzz')
+        else:
+            print(x)
