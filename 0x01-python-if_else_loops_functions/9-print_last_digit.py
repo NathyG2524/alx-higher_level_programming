cf69d366b7fde5754a518cc9ac1c5f6441c8440b
NathyG2524 <Nathygirma2524@gmail.com>
2021-11-16 15:09:40 -0800

    last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        lastd = ((number * -1) % 10) * -1
+        lastd = ((number * -1) % 10)
     else:
         lastd = number % 10
     print(lastd, end="")
