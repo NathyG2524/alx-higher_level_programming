19f9d6f9ee5fa05b5efe6c8787b7b1210361a922
NathyG2524 <Nathygirma2524@gmail.com>
2021-11-18 09:05:26 -0800

    argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import sys
 if __name__ == "__main__":
+    import sys
     if len(sys.argv) == 1:
         print(f"{len(sys.argv) - 1} arguments. ")
     elif len(sys.argv) == 2:
