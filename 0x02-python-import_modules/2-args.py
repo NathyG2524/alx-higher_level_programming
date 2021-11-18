418319ba2bd6f3f4319d31c8d777e742b2914c08
NathyG2524 <Nathygirma2524@gmail.com>
2021-11-18 06:50:21 -0800

    argv input command

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    if len(sys.argv) == 0:
+    if len(sys.argv) == 1:
         print(f"{len(sys.argv)} arguments. ")
     elif len(sys.argv) == 2:
         print(f"{len(sys.argv) - 1} argument: ")
