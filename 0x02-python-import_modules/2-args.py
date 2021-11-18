4cded0ac4d2388bff3e7f9c48a56d8f66254ec7e
NathyG2524 <Nathygirma2524@gmail.com>
2021-11-18 10:03:32 -0800

    argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import sys
-if len(sys.argv) == 1:
-    print(f"{len(sys.argv) - 1} arguments. ")
-elif len(sys.argv) == 2:
-    print(f"{len(sys.argv) -1} argument: ")
-else:
-    print(f"{len(sys.argv) - 1} arguments: ")
-for i  in range(1, len(sys.argv)):
-    print('{}: {}'.format(i, sys.argv[i]))
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        print(f"{len(sys.argv) - 1} arguments. ")
+    elif len(sys.argv) == 2:
+        print(f"{len(sys.argv) -1} argument: ")
+    else:
+        print(f"{len(sys.argv) - 1} arguments: ")
+    for i in range(1, len(sys.argv)):
+        print('{}: {}'.format(i, sys.argv[i]))
