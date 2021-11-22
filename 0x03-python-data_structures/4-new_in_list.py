9f8927dfbbc910dd12926bad3b7804e68b43acd3
NathyG2524 <Nathygirma2524@gmail.com>
2021-11-22 10:58:51 -0800

    data structure

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,9 +3,9 @@
 
 def new_in_list(my_list, idx, element):
     if idx < 0:
-        return None
+        return my_list
     elif idx > len(my_list) - 1:
-        return None
+        return my_list
     else:
         original = my_list.copy()
         original[idx] = element
