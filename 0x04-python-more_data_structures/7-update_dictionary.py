a7bc0e1445e6798ff7e349239a054eb076ace4a8
NathyG2524 <Nathygirma2524@gmail.com>
2021-11-25 10:54:34 -0800

    data structures

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    copy_dic = a_dictionary.copy()
-    copy_dic[key] = value
-    return copy_dic
+    a_dictionary[key] = value
+    return a_dictionary
