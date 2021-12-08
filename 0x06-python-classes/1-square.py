285fe6f1e8fb698aaf3504a8494d9f9e7de83032
NathyG2524 <Nathygirma2524@gmail.com>
2021-12-08 10:23:41 -0800

    classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""""Define Square"""
+
+
+class Square:
+    """Represents a rectangle whose sides are equal
+
+    Attributes:
+        __size (int): length of side of the square
+    """
+    def __init__(self, size):
+        """Square is initialized
+        Args:
+            size (int): length of side of the squar
+        """
+        self.__size = size
---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""""Define Square"""
+
+
+class Square:
+    """Represents a rectangle whose sides are equal
+
+    Attributes:
+        __size (int): length of side of the square
+    """
+    def __init__(self, size=0):
+        """Square is initialized
+        Args:
+            size (int): length of side of the squar
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if type(size) is int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+    def area(self):
+        """Calculate Area of square
+        Return:
+             Area of a square
+        """
+        return (self.__size * self.__size)
