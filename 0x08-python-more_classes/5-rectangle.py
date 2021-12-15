e85616453ce8f6e54d4ba38c0f92d956dc5448a7
NathyG2524 <Nathygirma2524@gmail.com>
2021-12-15 08:22:58 -0800

    rectangle

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -1,16 +1,38 @@
 #!/usr/bin/python3
+"""
+rectangle module
+a rectangle class module
+"""
 
 class Rectangle:
+    """
+    class: Rectangle
+    this is a class rectangle
+    """
     def __init__(self, width=0, height=0):
+        """
+        method: __init__
+        :param width:
+        :param height:
+        """
         self.height = height
         self.width = width
 
     @property
     def height(self):
+        """
+        method: get height
+        :return: Private height
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """
+        method: set height
+        :param value: get value to be set on the height
+        :return: Private height
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
@@ -20,10 +42,19 @@ class Rectangle:
 
     @property
     def width(self):
+        """
+        method: get width
+        :return: Private width
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """
+        method: set width
+        :param value: value to be set on width
+        :return: Private width
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -32,15 +63,27 @@ class Rectangle:
             self.__width = value
 
     def area(self):
+        """
+        method: calculate the area of the rectangle
+        :return: area
+        """
         return self.__width * self.__height
 
     def perimeter(self):
+        """
+        method: calculate the perimeter of a rectangle
+        :return: perimeter
+        """
         per = 2 * (self.__width + self.__height)
         if self.__width == 0 or self.__height == 0:
             per = 0
         return per
 
     def __str__(self):
+        """
+        method: __str__
+        :return: readable string to the user
+        """
         string = ""
         if self.__width != 0 and self.__height != 0:
             for i in range(self.__height):
@@ -50,7 +93,15 @@ class Rectangle:
         return string
 
     def __repr__(self):
+        """
+        method: __repr__
+        :return: official string representation of an object
+        """
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
 
     def __del__(self):
+        """
+        method: __del__
+        :return: delete instance of Rectangle
+        """
         print("Bye rectangle . . .")
