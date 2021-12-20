38ec507e4b55e55c0ae060ddd0274c3416d84804
NathyG2524 <Nathygirma2524@gmail.com>
2021-12-20 07:02:15 -0800

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Improve Geometry
+"""
+
+
+class BaseGeometry:
+    """
+    add public instance method: def area(self)
+    """
+    def area(self):
+        """
+        function: area
+        :return:
+        """
+        raise Exception("area() is not implemented")
