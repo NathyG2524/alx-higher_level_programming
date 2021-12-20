f1b509124b25f65a9078343ffd8e4d240f221073
NathyG2524 <Nathygirma2524@gmail.com>
2021-12-20 06:36:50 -0800

    same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+same class or inherit from
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    function: is_kind_of_class
+    :param obj:
+    :param a_class:
+    :return: True or False
+    """
+    return isinstance(obj, a_class)
