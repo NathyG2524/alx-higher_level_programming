836a01f961c94183388c48bff737f75502c8fdd5
NathyG2524 <Nathygirma2524@gmail.com>
2022-01-03 00:36:43 -0800

    save to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,3 +28,13 @@ class Base:
         if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ save to file"""
+        for i in (list_objs):
+            dic_1 = i.to_dictionary()
+
+            name = i.__class__.__name__ + ".json"
+            with open(name, "w") as f:
+                f.write(cls.to_json_string(dic_1))
