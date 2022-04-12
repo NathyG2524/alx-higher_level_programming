284d8167bbe531405d10179355333b5b41199973
root <nathygirma2524@gmail.com>
2022-04-12 12:42:03 -0700

    js web scraping

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// script that writes a string to a file
+
+const file = process.argv[2];
+const content = process.argv[3];
+const fs = require('fs');
+
+fs.writeFile(file, content, err => {
+  if (err) {
+    console.error(err);
+  }
+});
